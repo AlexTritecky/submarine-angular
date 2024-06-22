b7e9709806c972546e1d19ac633874fda5be4647
Alex Trytetskyi <alextritecky@gmail.com>
2024-06-22 17:55:12 +0300

    add home page

---
+++ src/app/services/svg-register/svg-register.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
+export const SVG_ICONS: { [key: string]: string } = {
+  'icon-logo': 'assets/icons/icon-logo.svg',
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SvgRegisterService {
+  constructor(private readonly iconRegistry: MatIconRegistry, private readonly sanitizer: DomSanitizer) {}
+
+  public registerSvg(): void {
+    Object.keys(SVG_ICONS).forEach((key) => {
+      this.iconRegistry.addSvgIcon(key, this.sanitizer.bypassSecurityTrustResourceUrl(SVG_ICONS[key]));
+    });
+  }
+}
