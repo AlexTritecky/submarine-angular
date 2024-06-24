b4f4d37e4045a1ab34d437dbb7c330f96625269c
Alex Triteckiy <alextritecky@gmail.com>
2024-06-24 16:16:19 +0300

    add carousel

--- src/app/partials/carousel/carousel.component.ts
+++ src/app/partials/carousel/carousel.component.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
 import { CarouselItem, carouselItems } from '../../models/carousel.model';
 import { MatIconModule } from '@angular/material/icon';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, NgStyle],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
 })
 export class CarouselComponent {
   items: CarouselItem[] = carouselItems;
   constructor() {}
+
+  currentStartIndex = 0;
+  itemsToShow = 5;
+
+  get visibleItems() {
+    return this.items.slice(this.currentStartIndex, this.currentStartIndex + this.itemsToShow);
+  }
+
+  next() {
+    if (this.currentStartIndex + this.itemsToShow < this.items.length) {
+      this.currentStartIndex++;
+    } else {
+      this.currentStartIndex = 0;
+    }
+  }
+
+  prev() {
+    if (this.currentStartIndex > 0) {
+      this.currentStartIndex--;
+    } else {
+      this.currentStartIndex = this.items.length - this.itemsToShow;
+    }
+  }
 }
