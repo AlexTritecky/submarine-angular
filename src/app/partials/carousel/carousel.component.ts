9a66161097ae8e3a726acbe2d20f5066450f2fbb
Alex Triteckiy <alextritecky@gmail.com>
2024-07-25 10:47:49 +0300

    add new animation

--- src/app/partials/carousel/carousel.component.ts
+++ src/app/partials/carousel/carousel.component.ts
@@ -2,46 +2,15 @@ import { Component } from '@angular/core';
 import { CarouselItem, carouselItems } from '../../models/carousel.model';
 import { MatIconModule } from '@angular/material/icon';
 import { NgFor, NgStyle } from '@angular/common';
-import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [MatIconModule, NgStyle, CarouselModule, NgFor],
+  imports: [MatIconModule, NgStyle],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
 })
 export class CarouselComponent {
   items: CarouselItem[] = carouselItems.sort((a, b) => a.order - b.order);
-
-  customOptions: OwlOptions = {
-    items: 4,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 1000,
-    autoplayHoverPause: true,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-
-    dots: false,
-    startPosition: 0,
-    navSpeed: 700,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 2,
-      },
-      740: {
-        items: 3,
-      },
-      940: {
-        items: 7,
-      },
-    },
-    nav: false,
-  };
   constructor() {}
 }
