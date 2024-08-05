20b8df97ef131e9bf267931c48b8b5831c21d399
Alex <alextritecky@gmail.com>
2024-08-05 12:40:16 +0300

    Add team load

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,25 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID, Renderer2 } from '@angular/core';
 import { HeaderComponent } from '../../partials/header/header.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { TeamMember, teamMembers } from '../../models/team.model';
-import { NgStyle } from '@angular/common';
+import { isPlatformBrowser, NgStyle } from '@angular/common';
+import { LoaderComponent } from '../../partials/loader/loader.component';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [HeaderComponent, MatIconModule, MatDialogModule, NgStyle],
+  imports: [HeaderComponent, MatIconModule, MatDialogModule, NgStyle, LoaderComponent],
   templateUrl: './team.component.html',
   styleUrl: './team.component.scss',
 })
-export class TeamComponent {
+export class TeamComponent implements OnInit {
   teams: TeamMember[] = teamMembers;
-  readonly memberlink = 'https://t.me/SUBMARINETEAM'
+  isLoading = true;
+  readonly memberlink = 'https://t.me/SUBMARINETEAM';
 
-  constructor() {
+  constructor(
+    private renderer: Renderer2,
+    @Inject(PLATFORM_ID) private platformId: Object,
+  ) {
     if (typeof window !== 'undefined') {
       window.scrollTo(0, 0);
     }
+  }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.loadBackgroundImage();
+    }
+  }
+
+  loadBackgroundImage() {
+    const backgroundImage = this.renderer.createElement('img');
+    this.renderer.setAttribute(backgroundImage, 'src', '/assets/images/hero-bg.webp');
+
+    this.renderer.listen(backgroundImage, 'load', () => {
+      // Background image is loaded, now load the team images
+      this.loadTeamImages();
+    });
+
+    this.renderer.listen(backgroundImage, 'error', () => {
+      // Handle background image loading error if needed
+      this.loadTeamImages(); // Continue loading other images even if background fails
+    });
+  }
+
+  loadTeamImages() {
+    const totalImages = this.teams.length;
+    let loadedImages = 0;
+
+    this.teams.forEach((team) => {
+      const img = this.renderer.createElement('img');
+      this.renderer.setAttribute(img, 'src', team.image);
+
+      this.renderer.listen(img, 'load', () => {
+        loadedImages++;
+        if (loadedImages === totalImages) {
+          this.isLoading = false;
+        }
+      });
+
+      this.renderer.listen(img, 'error', () => {
+        loadedImages++;
+        if (loadedImages === totalImages) {
+          this.isLoading = false;
+        }
+      });
+    });
   }
 
   becameMember() {
